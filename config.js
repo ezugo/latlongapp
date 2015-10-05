4a6844ac0d173d0866fa9ad0b55b08eaa181b985
Chukwuezugo <ezugo_nwosu@yahoo.com>
2015-10-05 08:15:08 -0700

    modified the mongodb to an online version called mongolab

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/latlongIOTapp',
+	'database': 'mongodb://admin:admin@ds029804.mongolab.com:29804/lat_long_iot_app-db',
 };
+
+// 'database': 'mongodb://localhost:27017/latlongIOTapp'
